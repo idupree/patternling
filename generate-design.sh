a3e2a03fd3562abbc3bdd52dafd63c574d2831d0
Izzy Vivian Dupree <antispam@idupree.com>
2016-10-27 15:59:06 -0400

    change background colour

--- generate-design.sh
+++ generate-design.sh
@@ -2,5 +2,5 @@
 set -eu
 node script.js > design.svg
 #inkscape --without-gui --export-png=design.png --export-background-opacity=0 -w 440 -h 220 design.svg
-convert -density 200 design.svg -background "#ccc" -flatten -resize 880x440 design.png
+convert -density 200 design.svg -background "#edc" -flatten -resize 880x440 design.png
 #convert -density 200 design.svg -resize 440x220 -transparent white design.png
